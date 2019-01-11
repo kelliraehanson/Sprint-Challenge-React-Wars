34cb3d96a8c4a8f342fab95ef7d4deba772170c7
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-11 12:58:11 -0600

    making changes to css

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,8 +35,7 @@ class App extends Component {
       <div className="App">
         <h1 className="Header">React Wars!</h1>
 
-        <img src="https://starwarsblog.starwars.com/wp-content/uploads/2015/10/tfa_poster_wide_header-1536x864-959818851016.jpg" alt="W3Schools.com">
-        
+        <img className="pic" src="https://starwarsblog.starwars.com/wp-content/uploads/2015/10/tfa_poster_wide_header-1536x864-959818851016.jpg" alt="starwars" id="pic">
         </img>
 
         <StarwarsList banana={this.state.starwarsChars} />
